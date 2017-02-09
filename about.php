029dc7611c2e5217342a6108c8c0a917e0f6b3cc
Daniel Lennon <dlennon@campbell.dreamhost.com>
2017-02-09 10:22:46 -0800

    first checkin

---
+++ about.php
@@ -0,0 +1,22 @@
+<html>
+<title>Siren Report</title>
+<body>
+<h1>About SFSiren.net</h1>
+<a href="./checkins.php">Checkins</a>
+<a href="./leaderboard.php">Leaderboard</a>
+<a href="./view_sirens.php">Siren List</a>
+<a href="./user.php">User Summary</a>
+<a href="./siren.php">Siren</a>
+<a href="./hello.php">Hello</a>
+
+<p>
+SFSiren.net is the brainchild and hobby of Dan Lennon (KK6VNY). After he heard that a friend was trying to audit all of the sirens for SF ACS's Siren Net he started wondering which sirens were most freqently checked on and how to determine which sirens nobody had paid attention to in a while.
+</p>
+<p>
+Naturally this led to the idea to stuff all the checkins he heard into a database and to start keeping track of which sirens were and weren't being checked. This lead to all kinds of questions about the people checking in on sirens (who does it the most?, or have people adopted one siren they check every week e.g. the one outside of your office.
+
+</p>
+
+
+</body>
+</html>
