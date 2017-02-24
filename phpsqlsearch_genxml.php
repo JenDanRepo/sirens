a606a3496683e3646b72696776d33f5163ca4323
Daniel Lennon <dlennon@campbell.dreamhost.com>
2017-02-23 19:40:34 -0800

    Converted to PDO

--- phpsqlsearch_genxml.php
+++ phpsqlsearch_genxml.php
@@ -2,36 +2,29 @@
 header("Content-type: text/xml");
 require("phpsqlsearch_dbinfo.php");
 
-// Get parameters from URL
-//$center_lat = $_GET["lat"];
-
-
 // Start XML file, create parent node
 $dom = new DOMDocument("1.0");
 $node = $dom->createElement("markers");
 $parnode = $dom->appendChild($node);
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die('Connection failed: ' . $conn->connect_error);
-} 
-$callsign = mysqli_real_escape_string($conn, $_GET['callsign']);
+$pdostring = 'mysql:host=' . $servername .';dbname=' . $dbname .';charset=utf8mb4';
+$db = new PDO($pdostring, $username, $password);
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
-$sql = "SELECT number, address, CONCAT(number,\" - \",name) as name, lat, lng FROM Sirens WHERE number IN (SELECT siren FROM Checkins)";
+$callsign = $_GET['callsign'];
 
 // Find Sirens that $callsign has checked
 // should only do if callsign is not null
 
 if ($callsign !== ""){
 
-  $sql = "SELECT number, address, CONCAT(number,\" - \",name) as name, lat, lng FROM Sirens WHERE number IN (SELECT siren FROM Checkins WHERE callsign = \"$callsign\")";
-
-  $result = $conn->query($sql) or die(mysqli_error());
+  $stmt = $db->prepare('SELECT number, address, CONCAT(number," - ",name) as name, lat, lng FROM Sirens WHERE number IN (SELECT siren FROM Checkins WHERE callsign = ?)');
+  $stmt->execute(array($callsign));
+  $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
   // Iterate through the rows, adding XML nodes for each
-  while ($row = mysqli_fetch_array($result)){
+  while ($row = array_shift($rows)){
     $node = $dom->createElement("marker");
     $newnode = $parnode->appendChild($node);
     $newnode->setAttribute("id", $row['number']);
@@ -46,12 +39,13 @@ if ($callsign !== ""){
 
 
 // Find sirens that were checked by someone other than $callsign
-$sql = "SELECT number, address, CONCAT(number,\" - \",name) as name, lat, lng FROM Sirens WHERE number IN (SELECT siren FROM Checkins WHERE callsign <> \"$callsign\")";
 
-$result = $conn->query($sql) or die(mysqli_error());
+$stmt = $db->prepare('SELECT number, address, CONCAT(number," - ",name) as name, lat, lng FROM Sirens WHERE number IN (SELECT siren FROM Checkins WHERE callsign <> ? AND number NOT IN (SELECT siren from Checkins WHERE callsign = ?))');
+$stmt->execute(array($callsign, $callsign));
+$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 // Iterate through the rows, adding XML nodes for each
-while ($row = mysqli_fetch_array($result)){
+while ($row = array_shift($rows)){
   $node = $dom->createElement("marker");
   $newnode = $parnode->appendChild($node);
   $newnode->setAttribute("id", $row['number']);
@@ -60,15 +54,11 @@ while ($row = mysqli_fetch_array($result)){
   $newnode->setAttribute("lat", $row['lat']);
   $newnode->setAttribute("lng", $row['lng']);
   $newnode->setAttribute("type", "someone");
-  //echo "we're in the loop<br>";
 }
 
 // Find sirens that have not been checked
-$sql = "SELECT number, address, CONCAT(number,\" - \",name) as name, lat, lng FROM Sirens WHERE number NOT IN (SELECT siren FROM Checkins)";
 
-$result = $conn->query($sql) or die(mysqli_error());
-
-while ($row = mysqli_fetch_array($result)){
+foreach($db->query('SELECT number, address, CONCAT(number," - ",name) as name, lat, lng FROM Sirens WHERE number NOT IN (SELECT siren FROM Checkins)') as $row) {
   $node = $dom->createElement("marker");
   $newnode = $parnode->appendChild($node);
   $newnode->setAttribute("id", $row['number']);
@@ -77,7 +67,6 @@ while ($row = mysqli_fetch_array($result)){
   $newnode->setAttribute("lat", $row['lat']);
   $newnode->setAttribute("lng", $row['lng']);
   $newnode->setAttribute("type", "unchecked");
-  //echo "we're in the loop<br>";
 }
 
 echo $dom->saveXML();
